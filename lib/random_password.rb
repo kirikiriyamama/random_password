5630153f2de32992d58c4eb37f7b1efa00413997
kirikiriyamama <ponde.ponde.ponde@gmail.com>
2014-07-29 20:24:14 +0900

    Implement RandomPassword.generate

--- lib/random_password.rb
+++ lib/random_password.rb
@@ -1,5 +1,12 @@
+require "random_password/symbol_set"
 require "random_password/version"
 
 module RandomPassword
-  # Your code goes here...
+  class << self
+    def generate(symbol_sets: %i(lowercase numeral), length: 10, readable: false)
+      symbols  = symbol_sets.map{ |symbol_set| SymbolSet.send(symbol_set) }.flatten
+      symbols -= SymbolSet.unreadable if readable
+      symbols.sample(length).join
+    end
+  end
 end
---
+++ lib/random_password/symbol_set.rb
@@ -0,0 +1,25 @@
+module RandomPassword
+  module SymbolSet
+    class << self
+      def numeral
+        [*"0".."9"]
+      end
+
+      def lowercase
+        [*"a".."z"]
+      end
+
+      def uppercase
+        [*"A".."Z"]
+      end
+
+      def special
+        [*"!".."/", *":".."@", *"[".."`", *"{".."~"]
+      end
+
+      def unreadable
+        %w(0 o O 1 l I |)
+      end
+    end
+  end
+end
